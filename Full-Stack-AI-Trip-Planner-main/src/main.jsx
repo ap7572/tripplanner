c9e97052b8e256a93d31130e120665efeec03975
ap7572 <104268094+ap7572@users.noreply.github.com>
2025-01-25 16:04:46 +0530

    Update main.jsx

--- Full-Stack-AI-Trip-Planner-main/src/main.jsx
+++ Full-Stack-AI-Trip-Planner-main/src/main.jsx
@@ -30,11 +30,11 @@ const router = createBrowserRouter([
 }
 ])
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_AUTH_CLIENT_ID}>
+  
+    <GoogleOAuthProvider clientId>
       <Header/>
       <Toaster />
       <RouterProvider router={router}/>
     </GoogleOAuthProvider>
-  </React.StrictMode> ,
+  
 )
